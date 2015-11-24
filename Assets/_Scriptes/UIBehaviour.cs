5c21cbb78352ac915dda66cdc469f1556d169898
Andreas Haslinger <haslingerandreas@gmail.com>
2015-11-24 12:21:23 +0100

    Erste Kamera-Animationen Tests

--- Assets/_Scriptes/UIBehaviour.cs
+++ Assets/_Scriptes/UIBehaviour.cs
@@ -5,10 +5,35 @@ using UnityEngine.UI;
 public class UIBehaviour : MonoBehaviour {
 
 	Hashtable ht;
+	Hashtable ht2;
 
 	// Use this for initialization
 	void Start () {
-		StartCoroutine(ChangeText());
+
+		ht = new Hashtable();
+		ht.Add("x",GameObject.Find("Player").transform.position.x);
+		ht.Add("y",GameObject.Find("Player").transform.position.y);
+		ht.Add("z",GameObject.Find("Player").transform.position.z);
+		ht.Add("time",2);
+
+		Camera camera = GameObject.Find ("Main Camera").GetComponent<Camera> ();
+
+		ht2 = new Hashtable();
+		ht2.Add("from",camera.orthographicSize);
+		ht2.Add("to",0.1);
+		ht2.Add ("time", 2);
+		ht2.Add ("onupdatetarget", this.gameObject);
+		ht2.Add ("onupdate","changeCameraSize");
+
+		iTween.MoveTo(GameObject.Find ("Main Camera"), ht);
+		iTween.ValueTo (GameObject.Find ("Main Camera"), ht2);
+		iTween.RotateTo(GameObject.Find ("Main Camera"),iTween.Hash("y",90,"easetype",iTween.EaseType.easeInOutSine,"time",2));
+
+		//StartCoroutine(ChangeText());
+	}
+
+	void changeCameraSize(float value) {
+		GameObject.Find ("Main Camera").GetComponent<Camera>().orthographicSize = value;
 	}
 
 	IEnumerator ChangeText() {
