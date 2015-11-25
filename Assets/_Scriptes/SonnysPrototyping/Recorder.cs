308e46977b972543c9226611df3adac9a1e8add6
AlmostSonny <PSonnleitner@gmx.at>
2015-11-25 15:53:41 +0100

    Recorder Scripts, JETZT GIBTS INGAME ASOOOOOOOO

---
+++ Assets/_Scriptes/SonnysPrototyping/Recorder.cs
@@ -0,0 +1,100 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Recorder : MonoBehaviour {
+
+	private AudioSource aud;
+	// Use this for initialization
+	private bool micConnected = false;  
+	
+	//The maximum and minimum available recording frequencies  
+	private int minFreq;  
+	private int maxFreq;  
+	
+	//A handle to the attached AudioSource  
+	private AudioSource goAudioSource;
+	private AudioClip tempClip;
+	private int lastSample;
+	
+	//Use this for initialization  
+	void Start()   
+	{  
+		//Check if there is at least one microphone connected  
+		if(Microphone.devices.Length <= 0)  
+		{  
+			//Throw a warning message at the console if there isn't  
+			Debug.LogWarning("Microphone not connected!");  
+		}  
+		else //At least one microphone is present  
+		{  
+			//Set 'micConnected' to true  
+			micConnected = true;  
+			
+			//Get the default microphone recording capabilities  
+			Microphone.GetDeviceCaps(null, out minFreq, out maxFreq);  
+			
+			//According to the documentation, if minFreq and maxFreq are zero, the microphone supports any frequency...  
+			if(minFreq == 0 && maxFreq == 0)  
+			{  
+				//...meaning 44100 Hz can be used as the recording sampling rate  
+				maxFreq = 44100;  
+			}  
+			
+			//Get the attached AudioSource component  
+			goAudioSource = this.GetComponent<AudioSource>();  
+		}  
+	}  
+	
+	// Update is called once per frame
+	void OnGUI() {
+		//If there is a microphone  
+		if(micConnected)  
+		{  
+			//If the audio from any microphone isn't being captured  
+			if(!Microphone.IsRecording(null))  
+			{  
+				//Case the 'Record' button gets pressed  
+				if(GUI.Button(new Rect(Screen.width/2-100, Screen.height/2-25, 200, 50), "Record"))  
+				{  
+					//Start recording and store the audio captured from the microphone at the AudioClip in the AudioSource
+					//change 10 to whate ever Length you want
+					//goAudioSource.clip = Microphone.Start(null, true, 2, maxFreq);
+					tempClip = Microphone.Start(null, true, 20, maxFreq);
+				}  
+			}  
+			else //Recording is in progress  
+			{  
+				//Case the 'Stop and Play' button gets pressed  
+				if(GUI.Button(new Rect(Screen.width/2-100, Screen.height/2-25, 200, 50), "Stop and Play!"))  
+				{  
+					lastSample = Microphone.GetPosition(null);
+					Microphone.End(null); //Stop the audio recording
+					float[] samples = new float[lastSample * tempClip.channels];
+
+					tempClip.GetData(samples, 0);
+
+					int i = 0;
+					while (i < lastSample) {
+						samples[i] = samples[i] * 0.5F;
+						++i;
+					}
+
+					goAudioSource.clip = AudioClip.Create("sample",lastSample,tempClip.channels,44100,false,false);
+					goAudioSource.clip.SetData(samples, 0);
+
+					SavWav.Save("myfile", goAudioSource.clip);
+					print("Länge" + goAudioSource.clip.length);
+					goAudioSource.Play(); //Playback the recorded audio  
+				}  
+				
+				GUI.Label(new Rect(Screen.width/2-100, Screen.height/2+25, 200, 50), "Recording in progress...");  
+			}  
+		}  
+		else // No microphone  
+		{  
+			//Print a red "Microphone not connected!" message at the center of the screen  
+			GUI.contentColor = Color.red;  
+			GUI.Label(new Rect(Screen.width/2-100, Screen.height/2-25, 200, 50), "Microphone not connected!");  
+		}  
+	}
+}
