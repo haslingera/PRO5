38c80b8722a2364412fb87adca9900b17ea3450c
AlmostSonny <PSonnleitner@gmx.at>
2015-11-26 16:32:08 +0100

    CarScene

---
+++ Assets/_Scriptes/SonnysPrototyping/CarControlls.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CarControlls : MonoBehaviour {
+
+	private Rigidbody rigibi;
+
+	void Awake () {
+		AudioAnalyzer.Instance.Init ();
+		rigibi = GetComponent<Rigidbody> ();
+	}
+
+	void FixedUpdate () {
+		if (AudioAnalyzer.Instance.getPitch () >= 500) {
+			transform.Rotate(new Vector3 (0,1,0));
+		} else if (AudioAnalyzer.Instance.getPitch () < 500 && AudioAnalyzer.Instance.getPitch () > 100 ) {
+			transform.Rotate(new Vector3 (0,-1,0));
+		}
+
+		if (AudioAnalyzer.Instance.getMicLoudness () >= 35) {
+			rigibi.AddForce(new Vector3 (50,0,0));
+		
+		}
+	}
+}
