45a0b455ed3cdcf385e2e0c78b3c9c1d35181137
Wolfgang Kern <wolfi.kerni@gmail.com>
2015-11-26 13:04:06 +0100

    Road Scene Update

--- Assets/_Scriptes/CarSpawn.cs
+++ Assets/_Scriptes/CarSpawn.cs
@@ -7,41 +7,66 @@ public class CarSpawn : MonoBehaviour {
 	float speed;
 	Vector3 start;
 	Vector3 end;
-	bool onTheMove = false;
+	bool onTheMove = true;
+	GameObject clone;
+	string name;
 
 	// Use this for initialization
 	void Start () {
-
-		start = this.gameObject.transform.position;
-
-		player = GameObject.Find ("Player");
-
-		end = new Vector3 (this.gameObject.transform.position.x,this.gameObject.transform.position.y,-16f);
 	
+		start = this.gameObject.transform.position;
+		name = this.gameObject.name;
+		player = GameObject.Find ("Player");
+		end = new Vector3 (this.gameObject.transform.position.x,this.gameObject.transform.position.y,-16f);
+		iTween.Stop (this.gameObject);
+		iTween.Init (this.gameObject);
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-		if (!onTheMove) {
-
+		if (!moves()) {
 			speed = Random.Range (1, 3);
+			Debug.Log (gameObject.name);
 			iTween.MoveTo (this.gameObject, iTween.Hash ("z", -16, "easetype", "linear", "time", speed));
-			onTheMove = true;
+			this.onTheMove = false;
 		}
 
-		if(this.gameObject.transform.position.z == -16){
-
+		/*if((int)this.gameObject.transform.position.z == Random.Range(-4,2)){
 			spawnCar();
-
+		}*/
+		if(this.gameObject.transform.position.z == -16){
+			spawnCar();
+			destroyCar();
 		}
 	}
 
 	void spawnCar(){
-		string temp = this.gameObject.name;
-		GameObject clone = Instantiate (this.gameObject, this.start, transform.rotation) as GameObject;
+
+		clone = Instantiate (this.gameObject, this.start, transform.rotation) as GameObject;
+		//iTween.Stop(this.gameObject);
+		//clone.GetComponent<CarSpawn>().setOnMove(false);
+		//iTween.MoveTo (clone, iTween.Hash ("z", -16, "easetype", "linear", "time", speed));
+		clone.name = name;
+	}
+
+	void destroyCar(){
+
 		Destroy (this.gameObject);
-		clone.name = temp;
+		onTheMove = true;
 
 	}
+
+	void setOnMove(bool x){
+		this.onTheMove = x;
+	}
+
+	bool moves(){
+		if (this.gameObject.transform.position == start)
+			return false;
+		else
+			return true;
+	}
+
 }
