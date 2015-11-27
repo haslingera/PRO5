b7811a3ba83af664c97a103769cedfbcf455efac
Wolfgang Kern <wolfi.kerni@gmail.com>
2015-11-27 10:20:05 +0100

    Road Scene done!

--- Assets/_Scriptes/Player_Road_Scene.cs
+++ Assets/_Scriptes/Player_Road_Scene.cs
@@ -3,71 +3,47 @@ using System.Collections;
 
 public class Player_Road_Scene : MonoBehaviour {
 
-	public bool talkDirtyToMe = false;
-	int count = 1;
-	int blink;
-	SkinnedMeshRenderer skinnedMeshRendererEyes;
 	float db;
 	Vector3 start;
 	Vector3 end;
 	bool move = false;
+	Vector3 newPos;
 
 	// Use this for initialization
 	void Start () {
 
-		blink = Random.Range (70,200);
-		skinnedMeshRendererEyes = GameObject.Find ("Eyes").GetComponent<SkinnedMeshRenderer> ();
 		start = this.transform.position;
 		end = new Vector3 (20f, start.y,start.z);
+		newPos = start;
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-		Blink ();
-
 		db = AudioAnalyzer.Instance.getMicLoudness();
 
 		if (!move) {
 			if (db > 15f) {
-				iTween.MoveTo (this.gameObject, iTween.Hash ("x", 20, "easetype", "linear", "time", 4f));
+				newPos.x += 0.2f;
+				this.transform.position = newPos;
 				move = true;
 			}
 		} else {
 			if (db >15f) {
-				iTween.Stop (this.gameObject);
 				move = false;
 			}
 		}
-	
-	}
-
-	void Blink(){
-		
-		if (count > blink) {
-			skinnedMeshRendererEyes.SetBlendShapeWeight (4, 100);
-			skinnedMeshRendererEyes.SetBlendShapeWeight (5, 100);
-			if (count > blink+20){
-				count = 0;
-				blink = Random.Range (100,300);
-			}
-		} else {
-			skinnedMeshRendererEyes.SetBlendShapeWeight (4, 0);
-			skinnedMeshRendererEyes.SetBlendShapeWeight (5, 0);
-		}
-		
-		count++;
 	}
 
 	void Awake(){
 		AudioAnalyzer.Instance.Init ();
 	}
 
-	public void resetPlayer(){
+	public void  resetPlayer(){
 
-		iTween.Stop (this.gameObject);
 		this.transform.position = start;
+		newPos = start;
 
 	}
 }
