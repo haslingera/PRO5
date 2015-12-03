cadc51ce7677752133fa9e90b608004738e26419
Wolfgang Kern <wolfi.kerni@gmail.com>
2015-12-03 10:37:56 +0100

    Update_Runner

---
+++ Assets/_Scriptes/Runner_Final.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Runner_Final : MonoBehaviour {
+
+	private Rigidbody rigbi;
+	private InputAnalyser sound;
+	private float startTimer;
+	private float EndTimer;
+	private bool already = false;
+	private float[] height = new float[2];
+	
+	void Start () {
+		rigbi = GetComponent<Rigidbody> ();
+		height[0] = 0.13f;
+		height[1] = 2.1f;
+	}
+	
+
+	void FixedUpdate () {
+
+		if (AudioAnalyzer.Instance.getPitch() < 300f && AudioAnalyzer.Instance.getPitch() > 0f) {
+			duck ();
+		}
+
+		if (AudioAnalyzer.Instance.getPitch() > 500f) {
+			jump ();
+		}
+
+		if (Time.time - startTimer > 1f) {
+			already = false;
+			transform.localScale = new Vector3 (1f,1f,1f);
+		}
+
+		if (!already) {
+			startTimer = Time.time;
+		}
+
+	}
+	
+	private void jump() {
+		if (!already) {
+			//transform.Translate (Vector3.up * 100 * Time.deltaTime, Space.World);
+			rigbi.velocity += new Vector3(0f,5f,0f);
+			already = true;
+			startTimer = Time.time;
+		}
+	}
+
+	private void duck() {
+		if (!already) {
+			transform.localScale = new Vector3 (1f, 0.5f, 1f);
+			transform.position -= new Vector3 (0f, 0.5f, 0f);
+			already = true;
+			startTimer = Time.time;
+		}
+	}
+
+	void Awake(){
+		AudioAnalyzer.Instance.Init ();
+	}
+}
---
+++ Assets/_Scriptes/Runner_ObstSpawn.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Runner_ObstSpawn : MonoBehaviour {
+
+	private float[] height = new float[2];
+	private float end = -52;
+
+	// Use this for initialization
+	void Start () {
+		height[0] = 0.13f;
+		height[1] = 2.1f;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (transform.position.x == end) {
+			destroyObst();
+		}
+	
+	}
+
+	void destroyObst(){
+
+	}
+
+
+}
