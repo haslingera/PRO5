27833e2d3a8affcc4acce3042330e8bd4590b1c9
AlmostSonny <PSonnleitner@gmx.at>
2016-01-19 12:32:51 +0100

    tennis

--- Assets/_Scriptes/SonnysPrototyping/Tennis_racket.cs
+++ Assets/_Scriptes/SonnysPrototyping/Tennis_racket.cs
@@ -3,13 +3,17 @@ using System.Collections;
 
 public class Tennis_racket : MonoBehaviour {
 
+	private Quaternion target;
+
 	// Use this for initialization
 	void Start () {
-	
+		target = Quaternion.Euler (0, 15, 300);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (AudioAnalyzer.Instance.getMicLoudness() > 10) {
+			transform.rotation = Quaternion.Lerp (transform.rotation, target, 0.02F);
+		}
 	}
 }
