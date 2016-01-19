6368228d47f12ae2da5bad42af83a05bef350e4d
Wolfgang Kern <wolfi.kerni@gmail.com>
2016-01-19 11:40:22 +0100

    Präsentations-Szene mit Animation der Kappe und Figur!

---
+++ Assets/_Scriptes/CapPropeller.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CapPropeller : MonoBehaviour {
+
+    public float rot = 0.1f;
+    float rotChange = 0.0f;
+    Vector3 startL;
+    Vector3 startR;
+    GameObject left;
+    GameObject right;
+
+    // Use this for initialization
+    void Start () {
+
+        left = GameObject.Find("left_part");
+        right = GameObject.Find("right_part");
+
+        startL = left.transform.position;
+        startR = right.transform.position;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        //this.transform.rotation = Quaternion.EulerAngles(0.0f, rotChange, 0.0f);
+        left.transform.RotateAround(transform.position, transform.up, Time.deltaTime * 90f);
+        right.transform.RotateAround(transform.position, transform.up, Time.deltaTime * 90f);
+        rotChange += rot;
+
+        left.transform.position = startL;
+
+        right.transform.position = startR;
+    }
+}
