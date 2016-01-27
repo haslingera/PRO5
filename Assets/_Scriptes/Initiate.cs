fe0131434b41d379aa32c302b48ff8bfcfe60be7
Andreas Haslinger <haslingerandreas@gmail.com>
2016-01-27 16:52:24 +0100

    Plattform-Szenen Update

--- Assets/_Scriptes/Initiate.cs
+++ Assets/_Scriptes/Initiate.cs
@@ -4,13 +4,15 @@ using System.Collections;
 public class Initiate : MonoBehaviour {
 
 	bool first = true;
-
+	
 	// Use this for initialization
 	void Start () {
 		if (Application.loadedLevelName.Equals ("Road_Scene")) {
 			UIBehaviour.Instance.CameraStartRotation (new Vector3 (0f, 0f, 0f)).TimeBand(true).CameraStartSize (0.01f).LevelStart ();
 		} else if (Application.loadedLevelName.Equals ("TreeSawing")) {
 			UIBehaviour.Instance.CameraStartRotation (new Vector3 (-90f, 90f, 0f)).TimeBand(true).CameraStartSize (0.01f).LevelStart ();
+		} else if (Application.loadedLevelName.Equals ("Plattformen-Szene")) {
+			UIBehaviour.Instance.CameraStartRotation (new Vector3 (-16f, 90f, 10f)).TimeBand(true).CameraStartSize (0.01f).LevelStart ();
 		} else {
 			UIBehaviour.Instance.CameraStartRotation (new Vector3 (-90f, 90f, 0f)).TimeBand(true).CameraStartSize (0.01f).LevelStart ();
 		}
@@ -31,6 +33,8 @@ public class Initiate : MonoBehaviour {
 			UIBehaviour.Instance.CameraEndRotation (new Vector3 (0f, 0f, 0f)).CameraEndSize (0.01f).LevelEnd ();
 		} else if (Application.loadedLevelName.Equals ("TreeSawing")) {
 			UIBehaviour.Instance.CameraEndRotation (new Vector3 (-90f, 90f, 0f)).CameraEndSize (0.01f).LevelEnd ();
+		} else if (Application.loadedLevelName.Equals ("Plattformen-Szene")) {
+			UIBehaviour.Instance.CameraStartRotation (new Vector3 (-16f, 90f, 10f)).TimeBand(true).CameraStartSize (0.01f).LevelStart ();
 		} else {
 			UIBehaviour.Instance.CameraEndRotation (new Vector3 (0f, 0f, 0f)).CameraEndSize (0.01f).LevelEnd ();
 		}
