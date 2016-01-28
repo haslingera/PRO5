743420eacb9279268406c9fbda39cb0d39723eab
Florian Peinsold <f.peinsold@gmail.com>
2016-01-28 16:29:32 +0100

    removed not ready scenes from random-level-loading array

--- Assets/_Scriptes/GameLogic.cs
+++ Assets/_Scriptes/GameLogic.cs
@@ -320,7 +320,7 @@ public class GameLogic : MonoBehaviour {
 
 	private int numberOfLives;
 	private int numberOfLevelsCompleted;
-	private string[] levels = new string[] {"TreeSawing", "Tennis", "FlappyScream", "Road_Scene", "Plattformen-Szene", "Tod-Szene-Spiel", "JumpAndDuck", "GlassDestroying", "PedestrianScare", "BabyScream"};
+	private string[] levels = new string[] {"TreeSawing", "Tennis", "FlappyScream", "Road_Scene", "Plattformen-Szene", "Tod-Szene-Spiel", "JumpAndDuck", "GlassDestroying"};
 	private string nextLevel;
 
 	private bool showMainMenu = false;
