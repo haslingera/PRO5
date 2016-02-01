5ebdf73ef9e4ffc188f25a3e362f436a3cd40fe4
AlmostSonny <PSonnleitner@gmx.at>
2016-02-01 14:59:13 +0100

    Highscore scene

---
+++ Assets/HighScore.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HighScore : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<UnityEngine.UI.Text> ().text = PlayerPrefs.GetInt ("highscore").ToString();
+	}
+}
