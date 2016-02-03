ed4961b67bab678bcca24b67d90a93f6f8c794b4
Andreas Haslinger <haslingerandreas@gmail.com>
2016-02-03 14:46:23 +0100

    Ui font new things wow amazing things are new and not probably maybe okay

--- Assets/_Scriptes/Initiate.cs
+++ Assets/_Scriptes/Initiate.cs
@@ -8,21 +8,21 @@ public class Initiate : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		if (Application.loadedLevelName.Equals ("Road_Scene")) {
-			UIBehaviour.Instance.CameraStartRotation (new Vector3 (0f, -90f, 0f)).TimeBand(true).CameraStartSize (0.01f).LevelStart ();
+			UIBehaviour.Instance.CameraStartRotation (new Vector3 (0f, -90f, 0f)).CameraStartSize (0.01f).LevelStart ();
 		} else if (Application.loadedLevelName.Equals ("TreeSawing")) {
-			UIBehaviour.Instance.CameraStartRotation (new Vector3 (-90f, 90f, 0f)).TimeBand(true).CameraStartSize (0.01f).LevelStart ();
+			UIBehaviour.Instance.CameraStartRotation (new Vector3 (-90f, 90f, 0f)).CameraStartSize (0.01f).LevelStart ();
 		} else if (Application.loadedLevelName.Equals ("Plattformen-Szene")) {
-			UIBehaviour.Instance.CameraStartRotation (new Vector3 (-90f, 90f, 0f)).TimeBand(true).CameraStartSize (0.01f).LevelStart ();
+			UIBehaviour.Instance.CameraStartRotation (new Vector3 (-90f, 90f, 0f)).CameraStartSize (0.01f).LevelStart ();
 		} else if (Application.loadedLevelName.Equals ("FlappyScream")) {
-			UIBehaviour.Instance.CameraStartRotation (new Vector3 (90f, 0f, 0f)).TimeBand(true).CameraStartSize (0.01f).LevelStart ();
+			UIBehaviour.Instance.CameraStartRotation (new Vector3 (90f, 0f, 0f)).CameraStartSize (0.01f).LevelStart ();
 		} else if (Application.loadedLevelName.Equals ("Tod-Szene-Spiel")) {
-			UIBehaviour.Instance.CameraStartRotation (new Vector3 (26.3f, -40f, 0f)).TimeBand(true).CameraStartSize (0.01f).LevelStart ();
+			UIBehaviour.Instance.CameraStartRotation (new Vector3 (26.3f, -40f, 0f)).CameraStartSize (0.01f).LevelStart ();
 		} else if (Application.loadedLevelName.Equals ("GlassDestroying")) {
-			UIBehaviour.Instance.CameraStartRotation (new Vector3 (-90f, 16.27f, 0f)).TimeBand(true).CameraStartSize (0.01f).LevelStart ();
+			UIBehaviour.Instance.CameraStartRotation (new Vector3 (-90f, 16.27f, 0f)).CameraStartSize (0.01f).LevelStart ();
 		} else if (Application.loadedLevelName.Equals ("Tennis")) {
-			UIBehaviour.Instance.CameraStartRotation (new Vector3 (14f, -307f, 0f)).TimeBand(true).CameraStartSize (0.01f).LevelStart ();
+			UIBehaviour.Instance.CameraStartRotation (new Vector3 (14f, -307f, 0f)).CameraStartSize (0.01f).LevelStart ();
 		} else {
-			UIBehaviour.Instance.CameraStartRotation (new Vector3 (-90f, 90f, 0f)).TimeBand(true).CameraStartSize (0.01f).LevelStart ();
+			UIBehaviour.Instance.CameraStartRotation (new Vector3 (-90f, 90f, 0f)).CameraStartSize (0.01f).LevelStart ();
 		}
 
 		if (UIBehaviour.Instance.initateScript) {
